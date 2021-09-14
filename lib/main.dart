ac5673c504caada3d951f03583e48ca0307a07b9
Alejandro <alejo80805@hotmail.com>
2021-09-13 19:35:14 -0500

    solucion profesora

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'hola.dart';
+import 'description_places.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title: Text("Hola Alejandro"),
           ),
-          body: DescriptionPlace(),
+          body: DescriptionPlace("Bahamas", 5, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
 
         ) //MyHomePage(title: 'Flutter Demo Home Page'),
     );
